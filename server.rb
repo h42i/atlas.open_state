bf55495cb3ef94c79085f83579757f8c65bccaf9
danb <daniel@localhost.localdomain>
2013-11-03 17:02:50 +0100

    Fixed.

--- server.rb
+++ server.rb
@@ -1,14 +1,17 @@
 #!/usr/bin/env ruby
 require 'sinatra'
 
-$device = "/dev/ttyACM0"
-$pin_one = "F5"
-$pin_two = "F6"
+$device = "/dev/ttyGPIO"
+$pin_one = "B4"
+$pin_two = "B6"
 
 def get_open_state
   begin
     file = File.open($device, "w+")
+    
     file.write($pin_one + "=1\r\n")
+    file.write($pin_two + "=0\r\n")
+
     file.write($pin_two + "?\r\n")
         
     result = file.readline
