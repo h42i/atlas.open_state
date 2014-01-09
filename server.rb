111fa1c39ffc84bcdd70af610a0e8bc3c1f409b8
danb <daniel@localhost.localdomain>
2014-01-09 22:10:03 +0100

    Hm?

--- server.rb
+++ server.rb
@@ -1,21 +1,30 @@
 require 'socket'
 require './state'
 
-server = TCPServer.open(7876)
+$server = TCPServer.open(7876)
 
-loop {
-    Thread.start(server.accept) do |client|
-        last_state = get_open_state()
-        new_state = get_open_state()
+$last_state = get_open_state()
+$new_state = get_open_state()
 
-        while last_state == new_state
-            last_state = new_state
-            last_state = get_open_state()
+$sleep_time = 1.0
 
-            sleep(0.5)
+check_change = Thread.new do
+    loop do
+        $last_state = $new_state
+        $new_state = get_open_state()
+
+        sleep($wait_time)
+    end
+end
+
+loop do
+    Thread.start($server.accept) do |client|
+        while $new_state != $last_state
+            sleep($wait_time)
+            break
         end
 
-        client.puts(get_open_state() ? 0 : 1)
-        client.close
+        client.puts($new_state ? 0 : 1)
+        client.close()
     end
-}
+end
